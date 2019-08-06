94864fbb0930c203fd2684aee24016dc10e34b8f
feelj <feelingjos@163.com>
2019-08-06 20:04:36 +0800

    保存操作

--- app/src/main/java/com/feeljcode/wordlearn/WordAddActivity.java
+++ app/src/main/java/com/feeljcode/wordlearn/WordAddActivity.java
@@ -2,12 +2,15 @@ package com.feeljcode.wordlearn;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.feeljcode.wordlearn.entity.WordItem;
@@ -16,11 +19,26 @@ import com.feeljcode.wordlearn.utils.DataOperation;
 public class WordAddActivity extends AppCompatActivity {
 
     private Context context;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         context = this;
+
+        mHandler = new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                super.handleMessage(msg);
+
+                String tag = (String) msg.obj ;
+                if ("success".equals(tag)){
+                    finish();//关闭
+                }else if ("fail".equals(tag)){
+                    Toast.makeText(context, "保存失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);//remove title bar  即隐藏标题栏
         getSupportActionBar().hide();// 隐藏ActionBar
@@ -41,31 +59,36 @@ public class WordAddActivity extends AppCompatActivity {
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 EditText word = findViewById(R.id.word);
                 EditText type = findViewById(R.id.type);
                 EditText phoneticSymbol = findViewById(R.id.phoneticSymbol);
                 EditText translate = findViewById(R.id.translate);
-
                 String wordVal = word.getText().toString();
                 String typeVal = type.getText().toString();
                 String phoneticSymbolVal = phoneticSymbol.getText().toString();
                 String translateVal = translate.getText().toString();
-
                 if( null == wordVal || null == typeVal ||null == phoneticSymbolVal ||null == translateVal ){
                     Toast.makeText(context,"请补全信息!!",Toast.LENGTH_LONG).show();
                     return;
                 }
-
                 WordItem wordItem = new WordItem();
                 wordItem.setWord(wordVal);
                 wordItem.setTranslate(translateVal);
                 wordItem.setType(typeVal);
                 wordItem.setPhoneticSymbol(phoneticSymbolVal);
-
                 new Thread(() ->{
+                    try {
+                        DataOperation.addWordLocal(context,wordItem);
+                        Message message = new Message();
+                        message.obj = "success";//保存成功
+                        mHandler.sendMessage(message);
+                    }catch (Exception ex){
+                        ex.getStackTrace();
 
-                    DataOperation.addWordLocal(context,wordItem);
+                        Message message = new Message();
+                        message.obj = "fail";//保存失败
+                        mHandler.sendMessage(message);
+                    }
 
                 }).start();
 
